276e9e26c20f1ab26ed76911041712af3ae346cc
KrekcrRoot <shilenkok@bk.ru>
2024-09-23 11:06:52 +0300

    [+] Api doc

--- src/report/report.entity.ts
+++ src/report/report.entity.ts
@@ -1,22 +1,43 @@
 import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { UserEntity } from "../user/dto/user.entity";
 import { MachineEntity } from "../machine/dto/machine.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity('Reports')
 export class ReportEntity
 {
+  @ApiProperty({
+    example: '066d3d6c-5b89-4967-83b2-130b2821989f',
+    description: 'UUID of report',
+  })
   @PrimaryGeneratedColumn(`uuid`)
   uuid: string;
 
+  @ApiProperty({
+    example: UserEntity,
+    description: 'User reported'
+  })
   @ManyToOne(() => UserEntity, user => user.uuid)
   user: UserEntity;
 
+  @ApiProperty({
+    example: 'Lorem Ipsum se dolor',
+    description: 'User body report'
+  })
   @Column()
   body: string;
 
+  @ApiProperty({
+    example: MachineEntity,
+    description: 'Machine linked to report',
+  })
   @ManyToOne(() => MachineEntity, machine => machine.uuid)
   machine: MachineEntity;
 
+  @ApiProperty({
+    example: new Date(),
+    description: 'Time report created',
+  })
   @CreateDateColumn()
   created_at: Date;
 }
--- src/wash/dto/wash.entity.ts
+++ src/wash/dto/wash.entity.ts
@@ -1,31 +1,53 @@
 import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { UserEntity } from "../../user/dto/user.entity";
 import { MachineEntity } from "../../machine/dto/machine.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity('Washes')
 export class WashEntity {
 
+  @ApiProperty({
+    example: 42,
+    description: 'ID wash',
+  })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({
+    example: new Date(),
+    description: 'Time washing begin',
+  })
   @CreateDateColumn({
     type: 'timestamp',
     default: () => 'CURRENT_TIMESTAMP(6)',
   })
   time_begin: Date
 
+  @ApiProperty({
+    example: new Date(),
+    description: 'Time washing end',
+  })
   @Column({nullable: true, default: null})
   time_end?: Date
 
+  @ApiProperty({
+    example: MachineEntity,
+    description: 'Machine entity'
+  })
   @ManyToOne(() => MachineEntity, machine => machine.uuid)
   machine: MachineEntity;
 
+  @ApiProperty({
+    example: UserEntity,
+    description: 'User entity',
+  })
   @ManyToOne(() => UserEntity, user => user.uuid)
   user: UserEntity;
 
-  @ManyToOne(() => UserEntity, user => user.uuid, { nullable: true })
-  queued?: UserEntity;
-
+  @ApiProperty({
+    example: false,
+    description: 'Is washing end hardly'
+  })
   @Column({ default: false })
   hard_end: boolean;
 
