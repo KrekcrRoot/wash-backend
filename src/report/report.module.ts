eff407a801ffdb9e9ed1b9157885e0303d383d60
KrekcrRoot <shilenkok@bk.ru>
2024-10-04 19:27:35 +0300

    [~] fix

--- src/report/report.module.ts
+++ src/report/report.module.ts
@@ -6,11 +6,15 @@ import { DatabaseModule } from "../database/database.module";
 import { UserModule } from "../user/user.module";
 import { machineProviders } from "../machine/machine.providers";
 import { RelationModule } from "../relation/relation.module";
+import { WashService } from "../wash/wash.service";
+import { washProviders } from "../wash/wash.providers";
+import { orderProviders } from "../order/order.providers";
+import { ConnectionModule } from "../connection/connection.module";
 
 @Module({
-  imports: [DatabaseModule, UserModule, RelationModule],
+  imports: [DatabaseModule, UserModule, RelationModule, ConnectionModule],
   controllers: [ReportController],
-  providers: [ReportService, ...reportProviders, ...machineProviders],
+  providers: [ReportService, ...reportProviders, ...machineProviders, WashService, ...washProviders, ...orderProviders],
   exports: [ReportService],
 })
 export class ReportModule {}
