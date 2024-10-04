7ec04427dbb5f2b7b161d4d21f1f44496618401b
KrekcrRoot <shilenkok@bk.ru>
2024-10-04 18:53:41 +0300

    [+] env config

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -1,17 +1,20 @@
 
 import { DataSource } from 'typeorm';
+import { ConfigService } from "@nestjs/config";
 
 export const databaseProviders = [
   {
     provide: 'DATA_SOURCE',
-    useFactory: async () => {
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => {
+
       const dataSource = new DataSource({
         type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: 'root',
-        database: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
         entities: [
           __dirname + '/../**/*.entity{.ts,.js}',
         ],
